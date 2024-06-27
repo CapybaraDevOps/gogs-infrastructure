3f7d6427f79cc5b05c1ea5d3cfa700f73350e4c1
Andrii Kovalchuk <andriy.p.kovalchuk@gmail.com>
2024-06-27 21:08:58 +0300

    Resoursc 'disk' moved to vm

--- terraform/jfrog-cr/vm.tf
+++ terraform/jfrog-cr/vm.tf
@@ -38,12 +38,12 @@ resource "google_compute_address" "public_ip" {
   name = "${var.region}-${var.app}-jfrog-jcr-ip"
 }
 
-# resource "google_compute_disk" "disk_jfrog_jcr" {
-#   name        = "${var.region}-${var.app}-disk-jfrog-jcr"
-#   description = "Disk for Jfrog config"
-#   type        = "pd-ssd"
-#   size        = var.disk_size
-#   labels = {
-#     app = var.app
-#   }
-# }
+resource "google_compute_disk" "disk_jfrog_jcr" {
+  name        = "${var.region}-${var.app}-disk-jfrog-jcr"
+  description = "Disk for Jfrog config"
+  type        = "pd-ssd"
+  size        = var.disk_size
+  labels = {
+    app = var.app
+  }
+}
