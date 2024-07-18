99761c7c5c52ea479616382eba7864810838237e
Borys <wushatneen@gmail.com>
2024-07-18 19:20:26 +0300

    timeout

--- terraform/gogs-app/modules/gogs/helm_consul.tf
+++ terraform/gogs-app/modules/gogs/helm_consul.tf
@@ -3,7 +3,7 @@ resource "helm_release" "consul" {
   repository = "https://helm.releases.hashicorp.com"
   chart      = "consul"
   namespace  = kubernetes_namespace.consul.metadata[0].name
-  timeout    = 420
+  timeout    = 600
   depends_on = [helm_release.gogs-app]
   #version    = "1.2.0"
 
