a1d9c88d5c7ca56c199e0b403606a990e4b9d8af
Borys <wushatneen@gmail.com>
2024-07-18 18:49:18 +0300

    minmaxing *gigachad*

--- terraform/gogs-app/modules/gogs/vars.tf
+++ terraform/gogs-app/modules/gogs/vars.tf
@@ -20,11 +20,11 @@ variable "clusterName" {
 }
 variable "minNode" {
   description = "Minimum Node Count"
-  default     = "3"
+  default     = "2"
 }
 variable "maxNode" {
   description = "Maximum Node Count"
-  default     = "5"
+  default     = "2"
 }
 variable "machine_type" {
   description = "Node Instance machine type"
