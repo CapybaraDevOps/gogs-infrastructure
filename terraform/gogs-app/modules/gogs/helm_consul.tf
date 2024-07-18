9d7ab57a3dd23cfa328a89a4f7342ab884cfb8bd
Borys <wushatneen@gmail.com>
2024-07-18 15:42:47 +0300

    consul agent setup

---
+++ terraform/gogs-app/modules/gogs/helm_consul.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "consul" {
+  name       = "consul"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "consul"
+  namespace  = kubernetes_namespace.consul.metadata[0].name
+  #version    = "1.2.0"
+
+  values = [
+    file("values-v1.yaml")
+  ]
+  set {
+    name  = "global.datacenter"
+    value = "dev-capybara-01"
+  }
+}
